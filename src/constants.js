fbc93ab5621d3fa8ab3b8a7b6ecbedd31b92d07f
jotanarciso <joaoznarciso@gmail.com>
2022-11-08 19:29:28 -0300

    fix: flareact template repo

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ const USER_DOCUMENTS_PATH = path.join(os.homedir(), "documents");
 const AZION_FOLDER_NAME = "azion-edge-functions";
 const AZION_EDGE_FUNCTIONS_PATH = path.join(USER_DOCUMENTS_PATH, AZION_FOLDER_NAME);
 const SUPPORTED_FRAMEWORKS = {
-  Flareact: "git@github.com:aziontech/flareact4azion-template.git",
+  Flareact: "git@github.com:jotanarciso/azion-flareact-template.git",
 };
 
 module.exports = {
